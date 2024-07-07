3dacf9e4d8429be3582e27c12c27bbc851d4763d
mrlazyt <gladandenis@gmail.com>
2024-07-07 03:04:19 +0300

    complete

--- task_2/src/App.js
+++ task_2/src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import TaskList from './components/tasks/TaskList';
-import Task from './components/tasks/Task';
 import TaskLayout from './components/tasks/TaskLayout';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path = "/" element = {<TaskLayout/>}>
             <Route index element = {<TaskList/>}/>
-            <Route path = "tasks/:id" element = {<Task/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
